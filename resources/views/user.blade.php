d1c15cbc626ac05c00b7167d0ed56678d0465717
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-02-26 16:56:30 +0700

    add: penerapan Eloquent ORM

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,12 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Information</title>
+    <title>Data User</title>
 </head>
 <body>
-    <h1>Personal Information</h1>
-    <br>
-    <p>Name     : {{ $name }}</p>
-    <p>ID       : {{ $id }}</p>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>User</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
