d105ec68815c2c8ec21e26f1b5cb738ab1e93184
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-03-05 07:54:43 +0700

    add: penerapan Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,19 +12,23 @@ class UserController extends Controller
 {
    public function index() {
         
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [   
-                // 'username'  => 'manager',
-                // 'nama'  => 'Manager',
-
-                'username'  => 'manager33',
-                'nama'  => 'Manager Tiga Tiga',
+                'username'  => 'manager11',
+                'nama'  => 'Manager11',
                 'password'  => Hash::make('12345'),
                 'level_id'  => 2
             ]
         );
-        $user->save();
+        
+        $user->username = 'manager 12';
 
-        return view('user', ['data' => $user]);
+        $user->save();
+        
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
     }
 }
