b745f2b18f1f00d010c18fd0ce28e883c0d18103
Hanif Faishal Hilmi <haniffaishalh@gmail.com>
2025-03-05 07:16:04 +0700

    add: Penggunaan Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,30 +12,19 @@ class UserController extends Controller
 {
    public function index() {
         
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_dua',
-        //     'nama' => 'Manager Dua',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
+        $user = UserModel::firstOrNew(
+            [   
+                // 'username'  => 'manager',
+                // 'nama'  => 'Manager',
 
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-        // $user = UserModel::find(1);
-        // return view('user', ['data' => $user]);
+                'username'  => 'manager33',
+                'nama'  => 'Manager Tiga Tiga',
+                'password'  => Hash::make('12345'),
+                'level_id'  => 2
+            ]
+        );
+        $user->save();
 
-        // $user = UserModel::firstWhere('level_id', 1)->first();
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,12 +11,18 @@
     <table border="1" cellpadding="2" cellspacing="0">
         <thead>
             <tr>
-                <th>Jumlah Pengguna</th>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>Id Level Pengguna</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>{{ $data }}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
         </tbody>
     </table>
